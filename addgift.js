bbec96fce1f5f602ae9416e713f0430c93f2c20a
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2022-02-03 15:21:49 +0800

    Update addgift.js

--- addgift.js
+++ addgift.js
@@ -29,6 +29,7 @@ window.onload = function() {
 
     // 多礼物上传部分
     let fileObj = null;
+
     // 多礼物上传选中文件
     $('.icon').on('click', function () {
         // 触发文件提交
@@ -84,4 +85,83 @@ window.onload = function() {
     });
 
     // 单礼物上传部分
+    let imgurl = '';
+
+    // 选中图片
+    $('.info_box').on('click', function () {
+        // 选择图片
+        $('#imgUp').click();
+
+        // 判断文件存在并保存
+        $('#imgUp').change((e) => {
+            const file = e.target.files[0];
+
+            if(file) {
+                imgObj = file;
+            } else {
+                alert('请选择图片文件!');
+                return;
+            }
+
+            // 上传图片 获取图片回传url
+            let formdata = new FormData();
+            formdata.append('file', file);
+            formdata.append('name', file.name);
+            $.ajax({
+                url: 'https://www.yangxiangrui.xyz:9092/eduoss/fileoss',
+                type: 'POST',
+                async: true,
+                data: formdata,
+                contentType: false,
+                processData: false,
+                success: (e) => {
+                    if(e.success) {
+                        const {url} = e.data;
+                        imgurl = url;
+
+                        $('.img_box > img').attr('src', imgurl);
+                        $('.img_box').css('display', 'block');
+
+                        alert('图片上传成功！');
+                    } else {
+                        alert('图片上传失败\n请稍后再试');
+                        return;
+                    }
+                }
+            });
+        })
+    });
+
+    // button提交表单及图片
+    $('#update_single').on('click', function () {
+        // 获取表单各内容
+        const obj = {
+            buyurl: $('#gift_source').val(),
+            des: $('gift_info').val(),
+            tag: $('#gift_tags').val(),
+            title: $('#gift_name').val(),
+            url: imgurl,
+            price: $('#gift_price').val()
+        };
+
+        // 检测表单各值有效性: html策略替代
+
+        // 提交表单信息
+        let request = new XMLHttpRequest();
+
+        request.onreadystatechange = function() {
+            if(this.readyState === 4) {
+                const res = JSON.parse(this.responseText);
+                if(res.success) {
+                    alert('提交成功！');
+                } else {
+                    alert(res.message);
+                }
+            }
+        };
+
+        request.open('POST', 'https://www.yangxiangrui.xyz:9092/gift/gift/addGift', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(obj));
+    });
 }
